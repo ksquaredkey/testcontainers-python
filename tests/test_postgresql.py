ff979994b24a380fdb1056f90752f0a41b9a62e5
Ken Key <kenneth.key@joby.aero>
2022-12-11 12:05:20 -1000

    Add support for additional PostgreSQL Container env variables.
    Added interface for environment variables that the PostgreSQL container now
    supports:
    
    * POSTGRES_INITDB_ARGS
    * POSTGRES_INITDB_WALDIR
    * POSTGRES_HOST_AUTH_METHOD
    * PGDATA
    
    As a bonus, added tests for POSTGRES_INITDB_ARGS and POSTGRES_HOST_AUTH_METHOD.

---
+++ tests/test_postgresql.py
@@ -0,0 +1,35 @@
+import pytest
+import sqlalchemy
+# import time
+
+from testcontainers.postgres import PostgresContainer
+
+# >= v14 use scram-sha-256 by default, so test changing to MD5
+@pytest.mark.parametrize('version', ['14.5', '15.1'])
+def test_docker_run_postgresql_md5(version):
+    _docker_run_postgresql_test(version, "md5")
+
+
+# >= v10 to <v14 use md5 by default, so test changing to scram-sha-256.
+@pytest.mark.parametrize('version', ['12.8', '13.5'])
+def test_docker_run_postgresql_scram(version):
+    _docker_run_postgresql_test(version, "scram-sha-256")
+
+
+def _docker_run_postgresql_test(version, method):
+    with PostgresContainer(
+        image=f'postgres:{version}',
+        initdb_args=f'--auth-host={method}',
+        host_auth_method=f'{method}'
+    ).with_bind_ports(5432, 45432) as postgres:
+        engine = sqlalchemy.create_engine(postgres.get_connection_url())
+        with engine.connect() as conn:
+            test_query = "SELECT rolname, rolpassword FROM pg_authid WHERE rolname = 'test';"
+            result = conn.execute(test_query)
+            name, password = result.fetchone()
+
+            assert name == 'test'
+
+            # password_method = str(password[:len(method)])
+            # password_method = password_method.lower()
+            assert method == str(password[:len(method)]).lower()
